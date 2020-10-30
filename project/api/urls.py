495691635bee905128abe4ebfab6631d479ad29b
Vadim Nikolaev <defntvdm@gmail.com>
2020-10-31 02:36:36 +0500

    ListViews

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import login
+from .views import login, GroupListView, LanguageListView, TaskCheckListView, TaskListView, UserListView
 
 
 app_name = 'api'
 urlpatterns = [
+    path('groups/', GroupListView.as_view(), name='groups'),
+    path('languages/', LanguageListView.as_view(), name='languages'),
+    path('tasks/', TaskListView.as_view(), name='tasks'),
+    path('task_checks/', TaskCheckListView.as_view(), name='task_checks'),
+    path('users/', UserListView.as_view(), name='users'),
     path('login/', login, name='login'),
 ]
