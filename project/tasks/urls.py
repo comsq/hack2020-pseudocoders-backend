a0a1cc304e6580aeef32f6b2c4fca65f404cc6b8
Дима Старков <dvstark@yandex.ru>
2020-10-31 13:01:20 +0500

    A

--- project/tasks/urls.py
+++ project/tasks/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from .views import get_status, run_task
+from .views import (
+    get_status,
+    run_task,
+    start_editor,
+    stop_editor,
+    editor_status,
+)
 
 app_name = 'tasks'
 urlpatterns = [
+    path('editor/<int:user_id>/start/', start_editor, name='start_editor'),
+    path('editor/<int:user_id>/stop/', stop_editor, name='stop_editor'),
+    path('editor/<int:user_id>/status/', editor_status, name='editor_status'),
     path('<task_id>/', get_status, name='get_status'),
     path('', run_task, name='run_task'),
 ]
